c09ac3f11513e4be759c75c50e2458580013f8b0
MjRind <92913618+MjRind@users.noreply.github.com>
2021-10-21 12:57:20 +0500

    Create GIF-DIRECTORY.m

---
+++ GIF-DIRECTORY.m
@@ -0,0 +1,20 @@
+directory = dir('D:\Majid\*.jpg');
+for i = 1 : size(directory,1)
+    name = directory(i).name;
+    name = strcat('D:\Majid\',name);
+    img{i} = imread(name);
+    img{i} = im2double(img{i});
+end
+iter = 40;
+img1 = img{1};
+for i = 1 : size(img,2)
+    if i < size(img,2)
+        new = img{i+1} - img{i};
+    else
+        new = img1 - img{i};
+    end
+    new = new ./ iter;
+    for j = 1 : iter
+        img{i} = img{i} + new;
+        [im,map] = rgb2ind(img{i},256);
+end
